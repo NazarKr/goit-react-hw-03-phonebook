81881dc3aec5f3bceebd8e786526e7ff0efba457
NazarKr <nazarok1990@gmail.com>
2023-01-20 16:37:58 +0200

    Add item

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,82 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+import React, { Component } from 'react';
+import ContactsList from './ContactList/ContactsList';
+// import { nanoid } from 'nanoid'
+
+export class App extends Component {
+
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: ''
+  }
+
+  addContact = e => {
+    e.preventDefault();
+    // const contact = this.state;
+    // console.log(e.target.elements.name.value);
+    // console.log(e.target.elements.number.value);
+    // console.log(contact);
+
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts]
+    }))
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+        contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+  
+  render() {
+    const { contacts } = this.state;
+
+    return (
+    <div>
       React homework template
-    </div>
-  );
+      <h1>Phonebook</h1>
+
+      <form autoComplete="off">
+        <label htmlFor="name">
+          Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+<br/>
+          <label htmlFor="number">
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+<br/>
+          <button
+            type="submit"
+            onSubmit={this.addContact}
+          >Add contact</button>
+      </form>
+
+        <ContactsList
+          contacts={contacts}
+          onDeleteContact={this.deleteContact}
+        />
+
+      </div>
+    )
+  };
 };
---
+++ src/components/ContactList/ContactsList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const ContactsList = ({ contacts, onDeleteContact }) => {
+    return (
+        <ul>
+            {contacts.map(({ id, name, number }) => (
+                <li key={id}>
+                    <p>{name}</p>
+                    <p>{number}</p>
+                    <button
+                        type="submit"
+                        onClick={() => onDeleteContact(id)}
+                    >Delete</button>
+                </li>))}
+        </ul>
+    );
+};
+
+export default ContactsList;
+
