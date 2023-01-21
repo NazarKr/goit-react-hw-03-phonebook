c64cadb06e63b7fd8a5b9d2eb48056d3c5bd0fb6
NazarKr <nazarok1990@gmail.com>
2023-01-21 14:07:19 +0200

    add nano id

--- src/components/FormContact/FormicContact.jsx
+++ src/components/FormContact/FormicContact.jsx
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 import { Formik, Field, Form } from 'formik';
+import { nanoid } from 'nanoid'
+
+
+// const id = nanoid()
 
 class FormicContact extends Component {
+    state = {
+        id: '',
+        name: '',
+        number: ''
+    }
+
+    nameId = nanoid();
+    numberId = nanoid();
+
+    reset = () => {
+        this.setState({ name: '', number: '' })
+    };
+
 
     render() {
         return (
             <Formik
-                initialValues={{
-                    name: '',
-                    number: ''
-                }}
+                initialValues={this.state}
                 onSubmit={async (values) => {
                     await new Promise((r) => setTimeout(r, 500));
                     // alert(JSON.stringify(values, null, 2));
                     this.props.onSubmit(values);
-
                 }}
             >
                 <Form>
-                    <label htmlFor="Name">Name</label>
+                    <label htmlFor={this.nameId}>Name</label>
                     <Field
-                        id="name"
+                        id={this.nameId}
                         name="name"
                         placeholder="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         type="text"
@@ -28,9 +41,9 @@ class FormicContact extends Component {
                         required
                     />
 
-                    <label htmlFor="lastName">Number</label>
+                    <label htmlFor={this.numberId}>Number</label>
                     <Field
-                        id="number"
+                        id={this.numberId}
                         name="number"
                         placeholder="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         type="tel"
